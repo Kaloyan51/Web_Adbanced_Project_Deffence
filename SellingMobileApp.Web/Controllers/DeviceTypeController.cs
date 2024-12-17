b3f1aa649fc0aef4eee1a36ec817434a23ba26c1
kaloyan51 <kakoyan.ivanov@gmail.com>
2024-12-17 16:41:28 +0200

    Added 3 new pages and layout button for them

---
+++ SellingMobileApp.Web/Controllers/DeviceTypeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SellingMobileApp.Web.Controllers
+{
+    public class DeviceTypeController : BaseController
+    {
+        [HttpGet]
+        [Route("Phones")]
+        public IActionResult Phones()
+        {
+            return View("~/Views/MobilleApp/Phones.cshtml");
+        }
+
+        [HttpGet]
+        [Route("Tablets")]
+        public IActionResult Tablets()
+        {
+            return View("~/Views/MobilleApp/Tablets.cshtml");
+        }
+
+        [HttpGet]
+        [Route("Accessories")]
+        public IActionResult Accessories()
+        {
+            return View("~/Views/MobilleApp/Accessories.cshtml");
+        }
+    }
+}
